42a43b69d2f446ddd4c3c5b4b738f62c331260df
Travis Brown <travjbrown98@gmail.com>
2026-03-10 00:51:36 -0500

    feat: add Google Fonts configuration

---
+++ src/lib/fonts.ts
@@ -0,0 +1,19 @@
+import { Dancing_Script, DM_Sans, League_Spartan } from "next/font/google";
+
+export const dancingScript = Dancing_Script({
+  subsets: ["latin"],
+  variable: "--font-dancing-script",
+  display: "swap",
+});
+
+export const dmSans = DM_Sans({
+  subsets: ["latin"],
+  variable: "--font-dm-sans",
+  display: "swap",
+});
+
+export const leagueSpartan = League_Spartan({
+  subsets: ["latin"],
+  variable: "--font-league-spartan",
+  display: "swap",
+});
